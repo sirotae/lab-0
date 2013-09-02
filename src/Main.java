b042f4bd2962fa319998df22e6b4676246e28d90
sirotae <sirotae@gmail.com>
2013-09-02 21:10:55 +0300

    lab-0 commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Olena_Syrota
+ * Date: 9/2/13
+ * Time: 8:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+
+}
